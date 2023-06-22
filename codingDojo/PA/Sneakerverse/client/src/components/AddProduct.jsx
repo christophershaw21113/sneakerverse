d6a886295588a7fdb2a0b9328b99be38fd372c37
Chris Shaw <56316622+christophershaw21113@users.noreply.github.com>
2023-06-22 18:21:59 -0400

    Delete codingDojo/PA/Sneakerverse/client directory

--- codingDojo/PA/Sneakerverse/client/src/components/AddProduct.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddProduct = () => {
-    return (
-        <div style={{marginTop: "100px"}}>AddProduct</div>
-    )
-}
-
-export default AddProduct
--- codingDojo/PA/Sneakerverse/client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-    return (
-        <footer>SneakerVerse ® 2023 Coding Dojo Group Project</footer>
-    )
-}
-
-export default Footer
