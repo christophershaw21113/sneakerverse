f99d08b2c7ca2852046308f5931909fa40126e68
Chris Shaw <christophershaw2113@gmail.com>
2023-06-26 20:36:05 -0400

    Building home carousel, placeholder images for now

---
+++ client/src/components/ImageSlider.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+const ImageSlider = ({slides}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+      const isFirstSlide = currentIndex === 0;
+      const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+      setCurrentIndex(newIndex);
+  }
+
+  const goToNext = () => {
+      const isLastSlide = currentIndex === slides.length - 1
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+  }
+ const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+ }
+
+  const leftArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    left: "32px",
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  }
+
+  const rightArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    right: "32px",
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+
+  }
+   
+  const sliderStyles = {
+    height: '100%',
+    position: 'relative'
+
+  }
+
+  const slideStyles = {
+    width: '100%',
+    height: '100%',
+    border: 'none',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundImage: `url(${slides[currentIndex].url})`
+
+  }
+
+  const dotsContainerStyles = {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+
+  const dotStyles = {
+    cursor: 'pointer',
+    fontSize: '20px',
+    width: '8px',
+    height: '8px',
+    margin: '5px 10px',
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    transition: '0.3s'
+  }
+
+
+    return (
+    <div style={sliderStyles}>
+        <div style={leftArrowStyles} onClick={goToPrevious}>&#9664;</div>
+        <div style={rightArrowStyles} onClick={goToNext}>&#9654;</div>
+    <div style={slideStyles}></div>
+    <div style={dotsContainerStyles}>
+        {slides.map((slide, slideIndex) => (
+            <div style={dotStyles} key={slideIndex} onClick={() => goToSlide(slideIndex)}>&#9679;</div>
+        ))
+        }
+    </div>
+    </div>
+    )
+};
+
+export default ImageSlider;
