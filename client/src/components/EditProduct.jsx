5eac696a35297f9fa6603f4aef2ee30d9c6b6163
Chris Shaw <christophershaw2113@gmail.com>
2023-06-29 20:48:02 -0400

    edit product page

--- client/src/components/EditProduct.jsx
+++ client/src/components/EditProduct.jsx
@@ -70,68 +70,68 @@ const EditProduct = () => {
     }
 
     return (
-        <div style={{ marginTop: "150px" }}>
-            <h1>Edit Shoe Details</h1>
-            <form onSubmit={editShoe}>
+        <div className='admin-container'>
+            <h2 style={{ textAlign: 'center' }}>Edit Shoe Details</h2>
+            <form className='admin-dash' onSubmit={editShoe}>
                 {/* {oneShoe.oneShoe?.length < 2 ? <p className="text-danger">FE: Title must be at least 2 characters</p> : null} */}
-                {/* {errors.oneShoe ? <p className="text-danger">{errors.oneShoe.message}</p> : null} */}
-                <div>
+               
+               <div> {/* {errors.oneShoe ? <p className="text-danger">{errors.oneShoe.message}</p> : null} */}
+               <div className='form-divs'>
                     <label>Name</label>
                     {errors?.name ? <p style={{ color: "red" }}>{errors?.name.message}</p> : null}
                     <input type="text" name="name" value={oneShoe.name} onChange={handleChange} />
                 </div>
-                <div>
+                <div className='form-divs'>
                     <label>Brand</label>
                     {/* {errors?.brand ? <p style={{ color: "red" }}>{errors?.brand.message}</p> : null} */}
                     <input type="text" name="brand" value={oneShoe.brand} onChange={handleChange} required />
                 </div>
-                <div>
+                <div className='form-divs'>
                     <label>Color</label>
                     {/* {errors?.color ? <p style={{ color: "red" }}>{errors?.color.message}</p> : null} */}
                     <input type="text" name="color" value={oneShoe.color} onChange={handleChange} required />
                 </div>
+                </div>
                 <div>
+                <div className='form-divs'>
                     <label>Description</label>
                     {/* {errors?.description ? <p style={{ color: "red" }}>{errors?.description.message}</p> : null} */}
                     <input type="text" name="description" value={oneShoe.description} onChange={handleChange} required />
                 </div>
-                <div>
+                <div className='form-divs'>
                     <label>Gender</label>
                     {/* {errors?.gender ? <p style={{ color: "red" }}>{errors?.gender.message}</p> : null} */}
-                    <select name="gender" id="gender">
+                    <select style={{ width: '100%', marginTop: '10px' }} name="gender" id="gender">
                         <option value="M">M</option>
                         <option value="F">F</option>
                     </select>
                 </div>
-                <div>
-                    <label>Price</label>
-                    {/* {errors?.price ? <p style={{ color: "red" }}>{errors?.email.message}</p> : null} */}
-                    <input type="number" name="price" value={oneShoe.price} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Discounted Price</label>
-                    {/* {errors?.discountedPrice ? <p style={{ color: "red" }}>{errors?.discountedPrice.message}</p> : null} */}
-                    <input type="number" name="discountedPrice" value={oneShoe.discountedPrice} onChange={handleChange} />
-                </div>
-                <div>
+                <div className='form-divs'>
                     <label>Size</label>
                     {/* {errors?.size ? <p style={{ color: "red" }}>{errors?.size.message}</p> : null} */}
                     <input type="number" name="size" value={oneShoe.size} onChange={handleChange} required />
                 </div>
-                <div>
-                    <button type="submit">Submit</button>
+                </div>
+                 <div>
+                 <div className='form-divs'>
+                    <label>Price</label>
+                    {/* {errors?.price ? <p style={{ color: "red" }}>{errors?.email.message}</p> : null} */}
+                    <input type="number" name="price" value={oneShoe.price} onChange={handleChange} required />
+                </div>
+                <div className='form-divs'>
+                    <label>Discounted Price</label>
+                    {/* {errors?.discountedPrice ? <p style={{ color: "red" }}>{errors?.discountedPrice.message}</p> : null} */}
+                    <input type="number" name="discountedPrice" value={oneShoe.discountedPrice} onChange={handleChange} />
+                </div>
+                    <button style={{display: 'flex', justifyContent: 'center', backgroundColor: '#000', color:'#fff', padding: '10px 15px', width: '200px', border: 'none', borderRadius: '15px' }} type="submit">Update</button>
                 </div>
             </form>
             <br />
-            <form onSubmit={editShoeImage}>
-                <div>
-                    <label>Image</label>
+            <form onSubmit={editShoeImage} style={{textAlign:"center"}}>
+                  
                     {/* {errors?.image ? <p style={{ color: "red" }}>{errors?.image.message}</p> : null} */}
-                    <input type="file" name="image" accept="image/*" onChange={handleChange} />
-                </div>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
+                    <input style={{marginBottom: '10px', marginLeft: '50px'}} type="file" name="image" accept="image/*" onChange={handleChange} />
+                    <button style={{display: 'flex', justifyContent: 'center', backgroundColor: '#198754', color:'#fff', padding: '10px 15px', width: '200px', border: 'none', borderRadius: '15px' }} type="submit">Submit</button>
             </form>
         </div>
     )
